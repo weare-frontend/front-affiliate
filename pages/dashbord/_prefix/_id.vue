531bcb1c02d8519e5c334d254411515bf2a36cd4
smile <sng.daris@gmail.com>
2020-06-17 01:43:41 +0700

    [commit]

--- pages/dashbord/_prefix/_id.vue
+++ pages/dashbord/_prefix/_id.vue
@@ -162,7 +162,7 @@ export default {
         },
         slides: 3,
         userAccount: "",
-        linkAccount: "https://nuxtv10-982d0.web.app/dashbord/",
+        linkAccount: "https://nuxtv10-982d0.web.app/dashboard/",
         linkAffiliate: "",
         selected: [],
     }),
